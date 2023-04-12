b9a1cdeb2fcd118e73d22cbbcbee42de68537445
HOUDA LALAOUI MOUTAOUAKKIL <houdalalaoui115@gmail.com>
2023-04-12 19:26:19 +0000

    login signup

---
+++ app/src/main/java/com/example/login_and_sign_up/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.login_and_sign_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
